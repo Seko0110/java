89a163d6bc65c74e4e44f6b24cdebcac156fa9f9
Sodko02 <131846547+Sodko02@users.noreply.github.com>
2023-05-07 14:19:07 +0800

    javat

---
+++ javas.js
@@ -0,0 +1,39 @@
+// p=(a+b)*2
+// s=(a*b)
+// let a=2;
+// let b=4;
+// let p;
+// let s ;
+
+// p = (a + b) *2
+
+// s= a * b
+// console.log(p, s)
+
+// . Тэгш өнцөгт гурвалжны тайлбаг ол. томъёог google дэхлжжэ
+let h=5;
+let b=10;
+let s;
+
+ s = b * h / 2
+console.log( s )
+// number -too 
+// string- text
+// undefing - todorhoi bus baina
+// NaN - too bish baina oor utgatai baina 
+// null-hooson 
+// Boolean- hudal unen buyu true fals 
+// operator 
+Arithmetic oprators 
++
+-
+*
+**
+/
+%
+
+
+
+let a =  1;
+console.log(typeof(a))
+console.log(typeof(b))
